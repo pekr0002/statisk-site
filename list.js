da371335620714b43874b93d3a55991ef53437dc
pekr0002 <pekr0002@stud.kea.dk>
2025-02-03 13:52:13 +0100

    rettelse på fejl

--- list.js
+++ list.js
@@ -4,20 +4,17 @@ fetch(`https://kea-alt-del.dk/t7/api/products/`)
   .then((data) => showList(data));
 
 function showList(products) {
-  console.log(products);
-  let markup = "";
-  products
-    .map((product) => {
-      markup += `<a href="product.html" class="product">
+  const markup = products
+    .map(
+      (product) => `<a href="product.html" class="product">
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}" />
          
           <h4>${product.productdisplayname}</h4>
           <div class="price">
             <p >${product.price}dkk</p>
           </div>
-        </a>`;
-    })
+        </a>`
+    )
     .join("");
-  console.log(markup);
   productContainer.innerHTML = markup;
 }
